79f037343d8cc57d5eae989152e6044281028e00
Purushothaman G <officialpurushothaman@gmail.com>
2021-05-30 22:50:35 +0530

    none

--- server.py
+++ server.py
@@ -25,8 +25,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
-gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
-                    base_url=None)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///sena.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
