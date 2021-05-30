e7b342541374b08c36b77e6e6b0721381b1295b2
Purushothaman G <officialpurushothaman@gmail.com>
2021-05-30 22:08:48 +0530

    sqlite to postgres

--- server.py
+++ server.py
@@ -19,15 +19,16 @@ from functools import wraps
 from markupsafe import Markup
 from datetime import datetime
 import random
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
                     base_url=None)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sena.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
