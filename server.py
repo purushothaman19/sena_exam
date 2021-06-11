65f591666c3c7dfa371704cb401e4f6a297953c3
Purushothaman G <officialpurushothaman@gmail.com>
2021-06-11 09:50:40 +0530

    examinee_name without foreign key

--- server.py
+++ server.py
@@ -71,6 +71,7 @@ class User(UserMixin, db.Model):
 #     email = db.Column(db.String(100), unique=True)
 
 
+
 class Test15(db.Model):
     __tablename__ = "Test_15"
     user_id = db.Column(db.Integer, primary_key=True)
