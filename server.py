61880273df8018c7feaedc7aa5cf7597b676b510
Purushothaman G <officialpurushothaman@gmail.com>
2021-06-13 18:14:01 +0530

    test on evaluate

--- server.py
+++ server.py
@@ -763,7 +763,7 @@ def evaluate():
         else:
             final_result.append("Wrong")
 
-    return f"{final_result}"
+    return f"{final_result, answers, correct_answer}"
     st_answers = '#||#'.join(answers)
     f_result = "#||#".join(final_result)
 
