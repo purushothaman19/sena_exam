7bc9ad91e8885420725f3efdc110661251d6dac8
Purushothaman G <officialpurushothaman@gmail.com>
2021-06-13 12:02:35 +0530

    csv file renamed

--- server.py
+++ server.py
@@ -325,8 +325,8 @@ def exam():
         db.session.commit()
 
         if current_user.email in report15.keys():
-            report15.loc[0, current_user.email] = marks
-            report15.loc[1, current_user.email] = datetime.datetime.now()
+            report15.loc[1, current_user.email] = marks
+            report15.loc[2, current_user.email] = datetime.datetime.now()
             report15.to_csv("report15.csv", index=False)
 
         return redirect(url_for('home', warn="You have successfully completed the exam. Click results to see results."))
