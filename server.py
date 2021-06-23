738cf127b204d147ac64bd8ccdbedf62606bc0b6
Purushothaman Ganesan <71747442+purushothaman19@users.noreply.github.com>
2021-06-23 09:41:32 +0530

    Test 16 for not attendee

--- server.py
+++ server.py
@@ -173,7 +173,7 @@ verified_emails = [mail.strip() for mail in student_mails.keys()]
 exam_sites = {
 
     "15": ["June 14, 2021 10:00:00", "June 14, 2021 13:30:00"],
-    "16": ["June 21, 2021 10:00:00", "June 21, 2021 13:30:00"]
+    "16": ["June 23, 2021 10:00:00", "June 23, 2021 17:30:00"]
 
 }
 
